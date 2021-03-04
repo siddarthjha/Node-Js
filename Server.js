ccdbd44b0ef9d82df48283a5375a007e8262d4f8
Siddarth Jha <500062053@stu.upes.ac.in>
2021-03-04 17:46:52 +0530

    Updated with /projects

--- Server.js
+++ Server.js
@@ -73,6 +73,13 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('project.hbs', {
+		pageTitle: 'Projects Page',
+		name: 'Siddarth Jha'
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Server is up on port ${port} `);
 });
